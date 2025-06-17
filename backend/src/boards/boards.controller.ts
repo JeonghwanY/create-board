64f9b36dc727302275c6a8c8a69640f53e782b9f
Jeonghwan <jh327373@gmail.com>
2025-06-17 10:42:09 +0900

    crud db connect start

--- backend/src/boards/boards.controller.ts
+++ backend/src/boards/boards.controller.ts
@@ -9,34 +9,34 @@ export class BoardsController {
     constructor(private boardsService: BoardsService){}
 
 
-    @Get()
-    getAllBoard(): Board[]{
-        return this.boardsService.getAllBoards();
-    }
-    @Post()//response
-    @UsePipes(ValidationPipe)
-    createBoard(
-        @Body() createBoardDto: CreateBoardDto
-    ): Board{
-        return this.boardsService.createBoard(createBoardDto);    
-    }
-    @Get('/:id')
-    getBoardById(@Param('id') id: string): Board {
-        return this.boardsService.getBoardById(id)
-    }
+    // @Get()
+    // getAllBoard(): Board[]{
+    //     return this.boardsService.getAllBoards();
+    // }
+    // @Post()//response
+    // @UsePipes(ValidationPipe)
+    // createBoard(
+    //     @Body() createBoardDto: CreateBoardDto
+    // ): Board{
+    //     return this.boardsService.createBoard(createBoardDto);    
+    // }
+    // @Get('/:id')
+    // getBoardById(@Param('id') id: string): Board {
+    //     return this.boardsService.getBoardById(id)
+    // }
 
-    @Delete('/:id')
-    deleteBoard(@Param('id') id: string): void{
-        this.boardsService.deleteBoard(id);
-    }
+    // @Delete('/:id')
+    // deleteBoard(@Param('id') id: string): void{
+    //     this.boardsService.deleteBoard(id);
+    // }
 
-    @Patch('/:id/status')
-    updateBoardStatus(
-        @Param('id') id: string,
-        @Body('status',BoardStatusValidationPipe) status: BoardStatus
-    ) {
-        return this.boardsService.updateBoardStatus(id,status);
-    }
+    // @Patch('/:id/status')
+    // updateBoardStatus(
+    //     @Param('id') id: string,
+    //     @Body('status',BoardStatusValidationPipe) status: BoardStatus
+    // ) {
+    //     return this.boardsService.updateBoardStatus(id,status);
+    // }
 
     // @Get(':id')
     // findOne(@Param('id',ParseIntPipe) id: number){
--- backend/src/boards/configs/typeorm.config.ts
+++ backend/src/boards/configs/typeorm.config.ts
@@ -6,7 +6,7 @@ export const typeORMConfig : TypeOrmModuleOptions = {//type orm 모듈
     port: 5432,
     username: 'postgres',
     password: 'postgres',
-    database: 'board-app',
+    database: 'board_app',
     entities: [__dirname + '/../**/*.entity.{js,ts'],//엔티티를 이용해서 데이테베이스 테이블을 생성해 엔티티파일이 어디에 있는지 설정해준다
     synchronize: true // true 값을 주면 애플리케이션을 다시 실행할 때 엔티티안에서 수정된 컬럼의 길이 타입 변경값등을 해당 테이블을 Drop한 후 재생성한다.
 }
