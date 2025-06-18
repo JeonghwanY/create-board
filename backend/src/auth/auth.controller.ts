bc7c21aa176838b110cb14c27b28a31076ef5fbb
Jeonghwan <jh327373@gmail.com>
2025-06-18 20:21:36 +0900

    start-hwan2

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -5,7 +5,7 @@ import { AuthGuard } from '@nestjs/passport';
 import { GetUser } from './get-user.decorator';
 import { User } from './user.entity';
 
-
+//
 @Controller('auth')
 export class AuthController {
     constructor( private authService: AuthService){}
